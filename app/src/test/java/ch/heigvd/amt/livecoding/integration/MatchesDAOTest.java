8781643f0b076b962e89686c2621c9dabfd81777
Filipe Fortunato <filipe.fortunato@heig-vd.ch>
2019-11-06 23:27:58 +0100

    fix missing sql seeds, woops X2

--- app/src/test/java/ch/heigvd/amt/livecoding/integration/MatchesDAOTest.java
+++ app/src/test/java/ch/heigvd/amt/livecoding/integration/MatchesDAOTest.java
@@ -108,13 +108,14 @@ public class MatchesDAOTest {
         assertNotEquals(matchOriginal, matchModified);
     }
 
+    // mostly for code coverage completeness
     @Test
     @Transactional(TransactionMode.ROLLBACK)
     public void itShouldNotBreakWithInvalidValues() {
         assertNull(manager.createMatch(0, 0, 0, 0, 0, 0));
         assertNull(manager.createMatch(null));
         assertNull(manager.createMatch(new Match(1,0, 0, null, null, null, null)));
-        assertEquals(-1, manager.getMatchCountFromUser(0));
+        assertEquals(-1, manager.getMatchCountFromUser(-1));
     }
 
 
